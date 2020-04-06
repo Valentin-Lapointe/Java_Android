55c7119f7da5e358addae9e9ae22a2e9e71a381c
UlDev <46440468+UlDev-UlDev@users.noreply.github.com>
2020-04-06 11:12:37 +0200

    FIX toolbar

--- JAVAngers/app/src/main/java/com/uldev/javangers/Hero_menu.java
+++ JAVAngers/app/src/main/java/com/uldev/javangers/Hero_menu.java
@@ -13,17 +13,6 @@ public class Hero_menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hero_menu);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
