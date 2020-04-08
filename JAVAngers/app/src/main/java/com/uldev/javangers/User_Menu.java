28e10bed443d998d296c3e029763997af0b63c2f
UlDev <46440468+UlDev-UlDev@users.noreply.github.com>
2020-04-08 09:10:42 +0200

    add navigation for incident_declaration activity

--- JAVAngers/app/src/main/java/com/uldev/javangers/User_Menu.java
+++ JAVAngers/app/src/main/java/com/uldev/javangers/User_Menu.java
@@ -1,5 +1,6 @@
 package com.uldev.javangers;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -13,6 +14,13 @@ public class User_Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user__menu);
+    }
+
+    public void openIncidentActivity(View view){
+        int user_id = 1; // A REMPLACER PAR LE USER CONNECTE LORSQUE LA CONNEXION SERA FAITE
+        Intent myIntent = new Intent(User_Menu.this, incident_declaration.class);
+        myIntent.putExtra("user_id", user_id);
+        User_Menu.this.startActivity(myIntent);
     }
 
 }
--- JAVAngers/app/src/main/java/com/uldev/javangers/incident_declaration.java
+++ JAVAngers/app/src/main/java/com/uldev/javangers/incident_declaration.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 public class incident_declaration extends AppCompatActivity {
 
@@ -13,6 +14,12 @@ public class incident_declaration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_incident_declaration);
+    }
+
+    public void createIncident(View view) {
+        EditText EditText_titre = (EditText) findViewById (R.id.editText9);
+        EditText EditText_endroit = (EditText) findViewById (R.id.editText10);
+        EditText EditText_détails = (EditText) findViewById (R.id.editText12);
     }
 
 }
