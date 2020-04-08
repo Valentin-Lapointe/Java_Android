63feb918611953fd5e224a2141f04a55573a5653
Valentin_Lapointe <valentinlapointe99@gmail.com>
2020-04-08 09:11:43 +0200

    feat: ajout de la navigation de la page de connexion vers la page d'inscription

--- JAVAngers/app/src/main/java/com/uldev/javangers/MainActivity.java
+++ JAVAngers/app/src/main/java/com/uldev/javangers/MainActivity.java
@@ -1,17 +1,33 @@
 package com.uldev.javangers;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import androidx.navigation.NavController;
 
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent otherActivity = new Intent(getApplicationContext(), Inscription.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
     }
 
     public void TestConn(View view) {
