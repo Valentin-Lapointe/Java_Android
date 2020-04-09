fd9408a776370b11586c1bc74cfcab51f111fec9
KatherineBreton <katherine.breton@viacesi.fr>
2020-04-09 08:55:52 +0200

    Navigation menus

--- JAVAngers/app/src/main/java/com/uldev/javangers/User_Menu.java
+++ JAVAngers/app/src/main/java/com/uldev/javangers/User_Menu.java
@@ -7,20 +7,63 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class User_Menu extends AppCompatActivity {
+    private Button incident;
+    private Button litige;
+    private Button satisfaction;
+    private Button profil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user__menu);
-    }
 
-    public void openIncidentActivity(View view){
+        this.incident = (Button) findViewById(R.id.button5);
+        incident.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent otherActivity = new Intent(getApplicationContext(), incident_declaration.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
+
+//        this.litige = (Button) findViewById(R.id.button8);
+//        litige.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent otherActivity = new Intent(getApplicationContext(), Incident.class);
+//                startActivity(otherActivity);
+//                finish();
+//            }
+//        });
+
+//        this.satisfaction = (Button) findViewById(R.id.button7);
+//        satisfaction.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent otherActivity = new Intent(getApplicationContext(), Incident.class);
+//                startActivity(otherActivity);
+//                finish();
+//            }
+//        });
+
+//        this.profil = (Button) findViewById(R.id.button6);
+//        profil.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent otherActivity = new Intent(getApplicationContext(), Incident.class);
+//                startActivity(otherActivity);
+//                finish();
+//            }
+//        });
+    }
+        public void openIncidentActivity (View view){
         int user_id = 1; // A REMPLACER PAR LE USER CONNECTE LORSQUE LA CONNEXION SERA FAITE
         Intent myIntent = new Intent(User_Menu.this, incident_declaration.class);
         myIntent.putExtra("user_id", user_id);
         User_Menu.this.startActivity(myIntent);
     }
-
 }
--- JAVAngers/app/src/main/java/com/uldev/javangers/admin_menu.java
+++ JAVAngers/app/src/main/java/com/uldev/javangers/admin_menu.java
@@ -1,29 +1,54 @@
 package com.uldev.javangers;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class admin_menu extends AppCompatActivity {
 
+    private Button mission;
+    private Button crise;
+    private Button incidents;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_admin_menu);
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_menu);
+
+        this.mission = (Button) findViewById(R.id.button5);
+        mission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent otherActivity = new Intent(getApplicationContext(), Missions.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
+
+        this.crise = (Button) findViewById(R.id.button8);
+        crise.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent otherActivity = new Intent(getApplicationContext(), Incident.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
+
+        this.incidents = (Button) findViewById(R.id.button7);
+        incidents.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent otherActivity = new Intent(getApplicationContext(), Incidents.class);
+                startActivity(otherActivity);
+                finish();
+            }
+        });
     }
 
 }
